5abc5d98d114766f484deeecb009f94330584454
Mathias Roggeman <mathias.roggeman@hotmail.be>
2018-09-03 16:15:24 +0200

    Magic 8 ball express working
    Onthoud vorige gestelde vragen en geeft zelfde antwoord op de zelfde vragen

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(function(req,res,next){
   res.locals.userValue = null;
   next();
 })
-var answers = [ 'It is certain', 'It is decidedly so.', 'Without a doubt.','Reply hazy, try again','Ask again later.','My reply is no.',' My sources say no','Outlook not so good.'];
+var answers = [ 'It is certain', 'It is decidedly so.', 'Without a doubt.','Yes - definitely.',' You may rely on it.',' As I see it, yes.','Reply hazy, try again','Ask again later.','My reply is no.',' My sources say no','Outlook not so good.'];
 function getRandomArrayElements(arr, count) {
   var shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
   while (i-- > min) {
@@ -26,14 +26,28 @@ function getRandomArrayElements(arr, count) {
   return shuffled.slice(min);
 }
 
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch');
+}
+
   app.get('/', (req, res) => {
     res.render('magic8ball');
  });
 
 app.post('/answer', (req,res)  => {
- var answer = {
-  data :  getRandomArrayElements(answers,1)
-}
+  var randomAnswer = getRandomArrayElements(answers,1);
+  var question = req.body.userInput;
+  if(localStorage.getItem(question) == null){
+    var answer = {
+      data :  randomAnswer
+    }
+    localStorage.setItem(question, randomAnswer);
+  }
+  var answer = {
+    data :  localStorage.getItem(question)
+  }
+
 console.log(answer)
   res.render('magic8ball',{
     userValue : answer 
